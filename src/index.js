9446e81f73c0acdb6fbc7a340f8e29364006c48d
moudekarvivek <moudekarvivek@gmail.com>
2024-04-21 18:10:15 +0530

    Deploy github action for node correction version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`The App is listening on ${port}`) 
